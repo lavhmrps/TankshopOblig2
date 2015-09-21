0730710312b3873700dcd6889cb265f36749bcb8
Jørgen Kann <jorgen@kann.no>
2015-09-21 21:53:08 +0200

    HomeController.cs

--- Nettbutikk/Nettbutikk/Controllers/HomeController.cs
+++ Nettbutikk/Nettbutikk/Controllers/HomeController.cs
@@ -6,25 +6,8 @@ using System.Web.Mvc;
 
 namespace Nettbutikk.Controllers
 {
-    public class HomeController : Controller
+    public class BaseController : Controller
     {
-        public ActionResult Index()
-        {
-            return View();
-        }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
     }
 }
