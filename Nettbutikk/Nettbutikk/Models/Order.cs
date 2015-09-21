b9e7ef7742f35ff2425dfcecf2a6cc75bcb3df46
Jørgen Kann <jorgen@kann.no>
2015-09-21 14:33:11 +0200

    Initial project structure.

---
+++ Nettbutikk/Nettbutikk/Models/Order.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Nettbutikk.Models
+{
+    public class Order
+    {
+        public int Id
+        {
+            get;
+            set;
+        }
+
+        public DateTime Date
+        {
+            get;
+            set;
+        }
+
+        public OrderLine[] Lines {
+            get;
+            set;
+        }
+
+
+    }
+}
---
+++ Nettbutikk/Nettbutikk/Models/User.cs
@@ -0,0 +1,11 @@
+﻿namespace Nettbutikk.Models
+{
+    public class User
+    {
+        public int Id
+        {
+            get;
+            set;
+        }
+    }
+}
