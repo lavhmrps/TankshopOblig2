1a8d065ece426e43540283e9bb53f59a9cfaa56e
Jørgen Kann <jorgen@kann.no>
2015-09-21 20:43:07 +0200

    Base controller class is now truly the base.
    Moved and renamed HomeController to BaseController. This reflects it being
    the base for the entire project as well as having a role as the base for
    all new sitewide functionality.

---
+++ Nettbutikk/Nettbutikk/Controllers/BaseController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Nettbutikk.Controllers
+{
+    public class BaseController : Controller
+    {
+        /*
+        * Base instance for all controllers in this project.
+        * Makes adding and removing site-wide solutions such as
+        * sessions and authentication much easier.
+        */
+    }
+}
