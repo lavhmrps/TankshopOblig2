6332c704f4eaa2a2f568f9d077fa8efd6e710b33
Jørgen Kann <jorgen@kann.no>
2015-09-22 10:45:21 +0200

    Added some model-validation.

--- Nettbutikk/Nettbutikk/Models/User.cs
+++ Nettbutikk/Nettbutikk/Models/User.cs
@@ -1,31 +1,43 @@
-﻿namespace Nettbutikk.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+
+namespace Nettbutikk.Models
 {
     public class User
     {
+        [HiddenInput(DisplayValue = false)]
         public int Id
         {
             get;
             set;
         }
 
+        [Required]
         public string Name
         {
             get;
             set;
         }
 
+        [Required]
         public string Adress
         {
             get;
             set;
         }
 
+        [Required]
+        [UIHint("Tel")]
+        [DataType(DataType.PhoneNumber)]
         public string Telephone
         {
             get;
             set;
         }
 
+        [Required]
+        [UIHint("EmailAddress")]
+        [DataType(DataType.EmailAddress)]
         public string Email
         {
             get;
