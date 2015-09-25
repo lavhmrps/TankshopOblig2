2e0354dbfad7e6e8b60bfc8916c1571ac168a5eb
Jørgen Kann <jorgen@kann.no>
2015-09-25 13:40:36 +0200

    Added MSoft OWIN authentication framework
    Added authentication and authorization framework. Created initial role
    manager framework and added this to the base controller as well as adding
    a separate Db-context controller below that again for DB-centric
    controllers.

--- Nettbutikk/Nettbutikk/Controllers/BaseController.cs
+++ Nettbutikk/Nettbutikk/Controllers/BaseController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.AspNet.Identity.Owin;
+using Nettbutikk.Infrastructure;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,6 +10,15 @@ namespace Nettbutikk.Controllers
 {
     public class BaseController : Controller
     {
+        private ApplicationRoleManager _AppRoleManager = null;
+
+        protected ApplicationRoleManager AppRoleManager
+        {
+            get
+            {
+                return _AppRoleManager ?? Request.GetOwinContext().GetUserManager<ApplicationRoleManager>();
+            }
+        }
         /*
         * Base instance for all controllers in this project.
         * Makes adding and removing site-wide solutions such as
