bd3ce98d51d2014583514490a6523f5a593c8a83
Jørgen Kann <jorgen@kann.no>
2015-09-30 09:43:07 +0200

    Cleaned up logic in product admin

--- Nettbutikk/Nettbutikk/Controllers/Admin/ProductsAdminController.cs
+++ Nettbutikk/Nettbutikk/Controllers/Admin/ProductsAdminController.cs
@@ -4,64 +4,70 @@ using System.Threading.Tasks;
 using System.Net;
 using System.Web.Mvc;
 using Nettbutikk.Models;
+using Nettbutikk.Models.Bindings;
 
 namespace Nettbutikk.Controllers.Admin
 {
     [RoutePrefix("Admin")]
+    [Authorize(Roles = "Administrator,Manager,Owner")]
+    [ModelBinder(typeof(Product))]
     public class ProductsAdminController : ProductsController
     {
         // GET: Products/Create
-        [Authorize(Roles = "Administrator,Manager,Owner")]
         public ActionResult Create()
         {
-            ViewBag.CategoryId = new SelectList(db.Categories, "Id", "Name");
+            ViewBag.Categories = new SelectList(db.Categories, "Id", "Name");
+
             return View();
         }
 
         // POST: Products/Create
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        [Authorize(Roles = "Administrator,Manager,Owner")]
-        public async Task<ActionResult> Create([Bind(Include = "Id,Name,Price,Description,CategoryId")] Product product)
+        [HttpPost, ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create(CreateProduct product)
         {
             if (ModelState.IsValid)
             {
                 product.Id = Guid.NewGuid();
+
                 db.Products.Add(product);
                 await db.SaveChangesAsync();
+
                 return RedirectToAction("Index");
             }
 
-            ViewBag.CategoryId = new SelectList(db.Categories, "Id", "Name", product.CategoryId);
+            ViewBag.Categories = new SelectList(db.Categories, "Id", "Name");
+            ViewBag.Category = await db.Categories.FindAsync(product.CategoryId);
+
             return View(product);
         }
 
         // GET: Products/Edit/5
-        [Authorize(Roles = "Administrator,Manager,Owner")]
         public async Task<ActionResult> Edit(Guid? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
+
             Product product = await db.Products.FindAsync(id);
+
             if (product == null)
             {
                 return HttpNotFound();
             }
-            ViewBag.CategoryId = new SelectList(db.Categories, "Id", "Name", product.CategoryId);
+
+            ViewBag.Category = await db.Categories.FindAsync(product.CategoryId);
+
             return View(product);
         }
 
         // POST: Products/Edit/5
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        [Authorize(Roles = "Administrator,Manager,Owner")]
-        public async Task<ActionResult> Edit([Bind(Include = "Id,Name,Price,Description,CategoryId")] Product product)
+        [HttpPost, ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit(EditProduct product)
         {
             if (ModelState.IsValid)
             {
@@ -69,35 +75,40 @@ namespace Nettbutikk.Controllers.Admin
                 await db.SaveChangesAsync();
                 return RedirectToAction("Index");
             }
-            ViewBag.CategoryId = new SelectList(db.Categories, "Id", "Name", product.CategoryId);
+
+            ViewBag.Category = await db.Categories.FindAsync(product.CategoryId);
+
             return View(product);
         }
 
         // GET: Products/Delete/5
-        [Authorize(Roles = "Administrator,Manager,Owner")]
         public async Task<ActionResult> Delete(Guid? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
+
             Product product = await db.Products.FindAsync(id);
+
             if (product == null)
             {
                 return HttpNotFound();
             }
+
             return View(product);
         }
 
         // POST: Products/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        [Authorize(Roles = "Administrator,Manager,Owner")]
+        [HttpPost, ActionName("Delete"), ValidateAntiForgeryToken]
         public async Task<ActionResult> DeleteConfirmed(Guid id)
         {
             Product product = await db.Products.FindAsync(id);
+
             db.Products.Remove(product);
+
             await db.SaveChangesAsync();
+
             return RedirectToAction("Index");
         }
 
