4f5f034cd9e53a790bec6a29d648349285c8d7cd
Magnus Lunde <maglunde@gmail.com>
2015-10-06 23:53:20 +0200

    helu

--- Nettbutikk/Nettbutikk/Scripts/_references.js
+++
@@ -1,7 +0,0 @@
-﻿/// <autosync enabled="true" />
-/// <reference path="bootstrap.min.js" />
-/// <reference path="jquery.unobtrusive-ajax.min.js" />
-/// <reference path="jquery.validate.min.js" />
-/// <reference path="jquery.validate.unobtrusive.min.js" />
-/// <reference path="jquery-2.1.4.js" />
-/// <reference path="jquery-2.1.4.min.js" />
