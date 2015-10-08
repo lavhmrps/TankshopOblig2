d38a7888da6b68a8754f9d03680155282d023ef0
Magnus Lunde <maglunde@gmail.com>
2015-10-08 18:15:23 +0200

    tanks n engines

--- Oblig1_Nettbutikk/Migrations/Configuration.cs
+++ Oblig1_Nettbutikk/Migrations/Configuration.cs
@@ -10,7 +10,7 @@ namespace Oblig1_Nettbutikk.Migrations
     {
         public Configuration()
         {
-            AutomaticMigrationsEnabled = false;
+            AutomaticMigrationsEnabled = true;
             ContextKey = "Oblig1_Nettbutikk.Models.WebShopModel";
         }
 
@@ -19,18 +19,22 @@ namespace Oblig1_Nettbutikk.Migrations
             // Categories
             context.Categories.AddOrUpdate(new Category
             {
+                CategoryID=1,
                 Name="Tanks"                
             },
             new Category
             {
+                CategoryID = 2,
                 Name ="Engines"
             },
             new Category
             {
+                CategoryID = 3,
                 Name = "Guns"
             },
             new Category
             {
+                CategoryID = 4,
                 Name = "Kjøkkenutstyr"
             });
 
@@ -38,6 +42,7 @@ namespace Oblig1_Nettbutikk.Migrations
             context.Products.AddOrUpdate(
             new Product
             {
+                ProductID=1,
                 Name = "Tiger 1",
                 Price =500000,
                 Stock=5,
@@ -45,6 +50,7 @@ namespace Oblig1_Nettbutikk.Migrations
                 CategoryID=1
             }, new Product
             {
+                ProductID = 2,
                 Name = "Panther",
                 Price = 760000,
                 Stock = 5,
@@ -52,6 +58,7 @@ namespace Oblig1_Nettbutikk.Migrations
                 CategoryID = 1
             }, new Product
             {
+                ProductID = 3,
                 Name = "JagdPanther",
                 Price = 50000,
                 Stock = 5,
@@ -59,6 +66,7 @@ namespace Oblig1_Nettbutikk.Migrations
                 CategoryID = 1
             }, new Product
             {
+                ProductID = 4,
                 Name = "Sherman Firefly",
                 Price = 50000,
                 Stock = 5,
@@ -66,6 +74,7 @@ namespace Oblig1_Nettbutikk.Migrations
                 CategoryID = 1
             }, new Product
             {
+                ProductID = 5,
                 Name = "Churchill Tank",
                 Price = 50000,
                 Stock = 5,
@@ -73,6 +82,7 @@ namespace Oblig1_Nettbutikk.Migrations
                 CategoryID = 1
             }, new Product
             {
+                ProductID = 6,
                 Name = "Light Tank Mk VI",
                 Price = 50000,
                 Stock = 5,
@@ -80,6 +90,7 @@ namespace Oblig1_Nettbutikk.Migrations
                 CategoryID = 1
             }, new Product
             {
+                ProductID = 7,
                 Name = "M4 Sherman",
                 Price = 50000,
                 Stock = 5,
@@ -87,6 +98,7 @@ namespace Oblig1_Nettbutikk.Migrations
                 CategoryID = 1
             }, new Product
             {
+                ProductID = 8,
                 Name = "M26 Pershing",
                 Price = 50000,
                 Stock = 5,
@@ -94,12 +106,69 @@ namespace Oblig1_Nettbutikk.Migrations
                 CategoryID = 1
             }, new Product
             {
+                ProductID = 9,
                 Name = "M10 tank destroyer",
                 Price = 50000,
                 Stock = 5,
                 Description = "The M10 tank destroyer was a United States tank destroyer of World War II based on the chassis of the M4 Sherman tank fitted with the 3-inch (76.2 mm) Gun M7. Formally 3-inch Gun Motor Carriage, M10, it was numerically the most important U.S. tank destroyer of World War II and combined a reasonably potent anti-tank weapon with a turreted platform. Despite the introduction of more-powerful types as replacements, it remained in service until the end of the war, and its chassis was later reused with a new turret to create the M36 Jackson, which used a 90mm gun instead of the 76.2mm gun.",
                 CategoryID = 1
             });
+
+            // Engines
+            context.Products.AddOrUpdate(
+            new Product {
+                ProductID = 10,
+                Name = "Wright R-975",
+                Price = 47500,
+                Stock = 8,
+                Description = "The Wright R-975 Whirlwind was a series of nine-cylinder air-cooled radial aircraft engines built by the Wright Aeronautical division of Curtiss-Wright. These engines had a displacement of about 975 in³ (16.0 L) and power ratings of 300-450 hp (225-335 kW). They were the largest members of the Wright Whirlwind engine family to be produced commercially, and they were also the most numerous.",
+                CategoryID = 2
+            },
+             new Product
+             {
+                 ProductID = 11,
+                 Name = "Chrysler A57 multibank",
+                 Price = 68189,
+                 Stock = 48,
+                 Description = "Created in 1941 as America entered World War II, the A57 Multibank engine was born out of the necessity for a rear-mounted tank engine to be developed and produced, in the shortest time possible, for use in both the 109 examples built of the M3A4 Medium Tank, and the 7,499 examples built of the successor M4A4 Medium tank, each of which had lengthened hulls to accommodate them.",
+                 CategoryID = 2
+             },
+             new Product
+             {
+                 ProductID = 12,
+                 Name = "Ford GAA engine",
+                 Price = 41688,
+                 Stock = 15,
+                 Description = "The Ford GAA engine is an American all-aluminum 32-valve DOHC 60-degree V8 engine engineered and produced by the Ford Motor Company just before, and during, World War II. It featured twin Stromberg NA-Y5-G carburetors, dual magnetos and twin spark plugs making up a full dual ignition system, and crossflow induction. It displaces 1,100 cu in (18 l) and puts out well over 1,000 pound-feet (1,400 N·m) of torque from idle to 2600 rpm. The factory-rated output was 525 hp (391 kW) @ 2800 rpm. In terms of its capacity, the GAA was the largest mass-produced gasoline V8 engine in the world.",
+                 CategoryID = 2
+             },
+             new Product
+             {
+                 ProductID = 13,
+                 Name = "Kharkiv model V-2-34",
+                 Price = 198689,
+                 Stock = 81,
+                 Description = "The Kharkiv model V-2 was a Soviet diesel tank V-12 engine designed at the Kharkiv Locomotive Factory by Konstantin Chelpan and his team. It is found in the BT-7M (BT-8), T-34, KV, IS and IS-10 (T-10) tanks, and by extension, the vehicles based on them, such as the SU-85 and SU-100 tank destroyers based on the T-34 and the ISU-122 and ISU-152 self-propelled guns based on the IS-2. Throughout its production life, output ranged from roughly 450-700 hp. V-2 with revised hull mounts, fuel and cooling connectors and refined clutch, 1939. Used in the T-34, SU-85 and SU-100. It produced 500 hp @ 1800 RPM.",
+                 CategoryID = 2
+             },
+             new Product
+             {
+                 ProductID = 14,
+                 Name = "Kharkiv model V-2K",
+                 Price = 61695,
+                 Stock = 25,
+                 Description = "The Kharkiv model V-2 was a Soviet diesel tank V-12 engine designed at the Kharkiv Locomotive Factory by Konstantin Chelpan and his team. It is found in the BT-7M (BT-8), T-34, KV, IS and IS-10 (T-10) tanks, and by extension, the vehicles based on them, such as the SU-85 and SU-100 tank destroyers based on the T-34 and the ISU-122 and ISU-152 self-propelled guns based on the IS-2. Throughout its production life, output ranged from roughly 450-700 hp. V-2 with increased injection pressure and higher engine speed, 1939. Used in the KV-1 and KV-2. It produced 600 hp.",
+                 CategoryID = 2
+             },
+             new Product
+             {
+                 ProductID = 15,
+                 Name = "Kharkiv model V-2",
+                 Price = 41789,
+                 Stock = 78,
+                 Description = "The Kharkiv model V-2 was a Soviet diesel tank V-12 engine designed at the Kharkiv Locomotive Factory by Konstantin Chelpan and his team. It is found in the BT-7M (BT-8), T-34, KV, IS and IS-10 (T-10) tanks, and by extension, the vehicles based on them, such as the SU-85 and SU-100 tank destroyers based on the T-34 and the ISU-122 and ISU-152 self-propelled guns based on the IS-2. Throughout its production life, output ranged from roughly 450-700 hp. Initial production version, 1937. Used in the BT-7M (BT-8).",
+                 CategoryID = 2
+             });
 
 
             //  This method will be called after migrating to the latest version.
