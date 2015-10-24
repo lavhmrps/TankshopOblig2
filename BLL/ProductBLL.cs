493e392fe4aff63dfff8c9be9bc64dfe7ded4691
Magnus Lunde <maglunde@gmail.com>
2015-10-24 02:04:53 +0200

    Hjelpemetoder for Cookiecontroller

---
+++ BLL/ProductBLL.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Oblig1_Nettbutikk.Model;
+using Oblig1_Nettbutikk.DAL;
+
+namespace Oblig1_Nettbutikk.BLL
+{
+    public class ProductBLL : IProductLogic
+    {
+        private IProductRepo _repo;
+
+        public ProductBLL()
+        {
+            _repo = new ProductRepo();
+        }
+
+        public List<ProductModel> GetProducts(List<int> productIdList)
+        {
+            return _repo.GetProducts(productIdList);
+        }
+    }
+}
---
+++ DAL/IProductRepo.cs
@@ -0,0 +1,14 @@
+﻿using Oblig1_Nettbutikk.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oblig1_Nettbutikk.DAL
+{
+    public interface IProductRepo
+    {
+       List<ProductModel> GetProducts(List<int> productIdList);
+    }
+}
