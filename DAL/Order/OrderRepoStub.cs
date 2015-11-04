4635ea6bee89d5aba863afcc914fa1b05bd06a2f
Magnus Lunde <maglunde@gmail.com>
2015-11-04 16:50:35 +0100

    hei

---
+++ DAL/Order/OrderRepoStub.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Oblig1_Nettbutikk.Model;
+
+namespace Oblig1_Nettbutikk.DAL
+{
+    public class OrderRepoStub : IOrderRepo
+    {
+        public bool DeleteOrder(int orderId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<OrderModel> GetAllOrders()
+        {
+            throw new NotImplementedException();
+        }
+
+        public double GetOrderSumTotal(int orderId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public OrderModel GetReciept(int orderId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int PlaceOrder(OrderModel order)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool UpdateOrderline(OrderlineModel orderline)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
