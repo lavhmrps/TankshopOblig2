78dd03dab920e80e605f6bb04005bc89a9f685a9
Magnus Lunde <maglunde@gmail.com>
2015-11-05 21:06:47 +0100

    CustomerController ferdig

--- TankShopUnitTest/CustomerControllerTest.cs
+++ TankShopUnitTest/CustomerControllerTest.cs
@@ -163,24 +163,14 @@ namespace TankShopUnitTest
             }
         }
 
-        //[TestMethod]
-        //public void OrderPartial_List()
-        //{
-        //    // Arrange
-        //    var Controller = new CustomerController(new CustomerBLL(new CustomerRepoStub()));
-
-        //    // Act
-        //    var result = (PartialViewResult)Controller.OrdersPartial(1);
-
-        //    // Assert
-        //    Assert.AreEqual("", result.ViewName);
-        //}
-
         [TestMethod]
-        public void UpdateCustomer_ok()
+        public void UpdateCustomer_Update_Self_ok()
         {
             // Arrange
+            var SessionMock = new TestControllerBuilder();
             var Controller = new CustomerController(new CustomerBLL(new CustomerRepoStub()));
+            SessionMock.InitializeController(Controller);
+            Controller.Session["Email"] = "ole@gmail.com";
             var customerView = new CustomerView()
             {
                 CustomerId = 1,
@@ -200,12 +190,100 @@ namespace TankShopUnitTest
 
         }
 
+        [TestMethod]
+        public void UpdateCustomer_Admin_Update_Ok()
+        {
+            // Arrange
+            var SessionMock = new TestControllerBuilder();
+            var Controller = new CustomerController(new CustomerBLL(new CustomerRepoStub()));
+            SessionMock.InitializeController(Controller);
+            Controller.Session["Admin"] = true;
+            Controller.Session["Email"] = "admin";
+
+            var customerView = new CustomerView()
+            {
+                CustomerId = 1,
+                Email = "ole@gmail.com",
+                Firstname = "Ole",
+                Lastname = "Olsen",
+                Address = "Persveien 5",
+                Zipcode = "0123",
+                City = "Oslo"
+            };
+
+            // Act
+            var result = (bool)Controller.UpdateCustomerInfo(customerView);
+
+            // Assert
+            Assert.IsTrue(result);
+
+        }
+
+        [TestMethod]
+        public void UpdateCustomer_Not_Logged_In()
+        {
+            // Arrange
+            var SessionMock = new TestControllerBuilder();
+            var Controller = new CustomerController(new CustomerBLL(new CustomerRepoStub()));
+            SessionMock.InitializeController(Controller);
+            Controller.Session["Email"] = null;
+
+            var customerView = new CustomerView()
+            {
+                CustomerId = 1,
+                Email = "ole@gmail.com",
+                Firstname = "Ole",
+                Lastname = "Olsen",
+                Address = "Persveien 5",
+                Zipcode = "0123",
+                City = "Oslo"
+            };
+
+            // Act
+            var result = Controller.UpdateCustomerInfo(customerView);
+
+            // Assert
+            Assert.IsFalse(result);
+
+        }
+
+        [TestMethod]
+        public void UpdateCustomer_Update_Another_Customer()
+        {
+            // Arrange
+            var SessionMock = new TestControllerBuilder();
+            var Controller = new CustomerController(new CustomerBLL(new CustomerRepoStub()));
+            SessionMock.InitializeController(Controller);
+            Controller.Session["Email"] = "ikkeOle@gmail.com";
+
+            var customerView = new CustomerView()
+            {
+                CustomerId = 1,
+                Email = "ole@gmail.com",
+                Firstname = "Ole",
+                Lastname = "Olsen",
+                Address = "Persveien 5",
+                Zipcode = "0123",
+                City = "Oslo"
+            };
+
+            // Act
+            var result = Controller.UpdateCustomerInfo(customerView);
+
+            // Assert
+            Assert.IsFalse(result);
+
+        }
 
         [TestMethod]
         public void UpdateCustomer_modelerror()
         {
             // Arrange
+            var SessionMock = new TestControllerBuilder();
             var Controller = new CustomerController(new CustomerBLL(new CustomerRepoStub()));
+            SessionMock.InitializeController(Controller);
+            Controller.Session["Email"] = "ikkeOle@gmail.com";
+
             var customerView = new CustomerView()
             {
                 CustomerId = 1,
@@ -226,15 +304,20 @@ namespace TankShopUnitTest
         }
 
 
+
         [TestMethod]
         public void DeleteCustomer_ok()
         {
             // Arrange
+            var SessionMock = new TestControllerBuilder();
             var Controller = new CustomerController(new CustomerBLL(new CustomerRepoStub()));
+            SessionMock.InitializeController(Controller);
+            Controller.Session["Admin"] = true;
+            Controller.Session["Email"] = "ikkeOle@gmail.com";
             var email = "ole@gmail.com";
 
             // Act
-            var result = (bool)Controller.DeleteCustomer(email);
+            var result = Controller.DeleteCustomer(email);
 
             // Assert
             Assert.IsTrue(result);
@@ -261,6 +344,45 @@ namespace TankShopUnitTest
 
         }
 
-       
+        [TestMethod]
+        public void DeleteCustomer_Not_Admin()
+        {
+            // Arrange
+            var SessionMock = new TestControllerBuilder();
+            var Controller = new CustomerController(new CustomerBLL(new CustomerRepoStub()));
+            SessionMock.InitializeController(Controller);
+            Controller.Session["Admin"] = false;
+            Controller.Session["Email"] = "ikkeOle@gmail.com";
+            var email = "ole@gmail.com";
+
+            // Act
+            var result = (bool)Controller.DeleteCustomer(email);
+
+            // Assert
+            Assert.IsFalse(result);
+
+
+        }
+
+        [TestMethod]
+        public void DeleteCustomer_Delete_Self_Not_Allowed()
+        {
+            // Arrange
+            var SessionMock = new TestControllerBuilder();
+            var Controller = new CustomerController(new CustomerBLL(new CustomerRepoStub()));
+            SessionMock.InitializeController(Controller);
+            Controller.Session["Admin"] = true;
+            Controller.Session["Email"] = "admin";
+            var email = "admin";
+
+            // Act
+            var result = (bool)Controller.DeleteCustomer(email);
+
+            // Assert
+            Assert.IsFalse(result);
+
+
+        }
+
     }
 }
