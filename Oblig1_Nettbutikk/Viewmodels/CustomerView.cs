b62de56caed06abc4bf1bcf0daf7e1db23396a32
Magnus Lunde <maglunde@gmail.com>
2015-11-08 20:07:51 +0100

    Displayname på ViewModeller Models til Viewmodels

--- Oblig1_Nettbutikk/Viewmodels/CustomerView.cs
+++ Oblig1_Nettbutikk/Viewmodels/CustomerView.cs
@@ -5,7 +5,7 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
-namespace Nettbutikk.Models
+namespace Nettbutikk.Viewmodels
 {
     public class CustomerView
     {
